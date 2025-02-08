9fc351a3897341b8fc8f0a647bf3e7eea0901ea3
Gpocas <gpocas01@gmail.com>
2025-02-07 21:48:35 -0300

    adjust project to build package (briefcase)

--- ds_python/main.py
+++ ds_python/main.py
@@ -2,26 +2,27 @@ import json
 import time
 
 from rich.console import Console
-from ui import create_ui
-from utils import run_stats
+from ds_python.ui import create_ui
+from ds_python.utils import run_stats
 
 console = Console()
 
-while True:
-    try:
-        stats = run_stats()
-        lines = stats.stdout.split('\n')
-        with console.status('Waiting for Containers...'):
-            while len(lines) <= 1 and '' in lines:
-                console.clear()
-                time.sleep(1)
-                stats = run_stats()
-                lines = stats.stdout.split('\n')
+def main():
+    while True:
+        try:
+            stats = run_stats()
+            lines = stats.stdout.split('\n')
+            with console.status('Waiting for Containers...'):
+                while len(lines) <= 1 and '' in lines:
+                    console.clear()
+                    time.sleep(1)
+                    stats = run_stats()
+                    lines = stats.stdout.split('\n')
 
-        lines = [json.loads(line) for line in lines if line]
-        ui = create_ui(lines)
-        ui.display()
-        time.sleep(0.5)
+            lines = [json.loads(line) for line in lines if line]
+            ui = create_ui(lines)
+            ui.display()
+            time.sleep(0.5)
 
-    except KeyboardInterrupt:
-        break
+        except KeyboardInterrupt:
+            break
